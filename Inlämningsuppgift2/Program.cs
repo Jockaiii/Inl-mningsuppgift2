3d9aa3636e52c77537db9af322375b30678e2398
00joaand <00jooand@gafe.molndal.se>
2020-09-16 14:36:51 +0200

    fixade 2 typos i while vilkoret för operatörer

--- Inlämningsuppgift2/Program.cs
+++ Inlämningsuppgift2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Dynamic;
 using System.Linq.Expressions;
 
@@ -9,10 +10,9 @@ namespace Inlämningsuppgift2
     {
         static void Main(string[] args)
         {
-            int amount_operators=0;
+            int amount_operators = 0;
 
-            string[] operators = new string[amount_operators];
-            int[] values = new int[amount_operators + 1];
+            
 
             string allowed_operators = "+-*/";
 
@@ -29,6 +29,12 @@ namespace Inlämningsuppgift2
 
             }
 
+            string[] operators = new string[amount_operators];
+            int[] values = new int[amount_operators + 1];
+
+            //get_input.Get_operator("Hej");
+            //get_input.Get_value(1);
+
             for (int i = 0; i < amount_operators; i++)
             {
                 do
@@ -39,19 +45,19 @@ namespace Inlämningsuppgift2
                         operators[i] = Console.ReadLine();
 
                     }
-                    catch (Exception)
+                    catch (FormatException)
                     {
                         Console.Write("Du har angivet ett felaktivt värde. Vänligen skriv in en operatör: ");
                         operators[i] = Console.ReadLine();
                     }
-                } while (operators[i] != allowed_operators && operators[i].Length != 1);
+                } while (operators[i].Contains(allowed_operators) && operators[i].Length == 1);
             }
 
             for (int i = 0; i < amount_operators + 1; i++)
             {
                 try
                 {
-                    Console.Write("Ange ett värde");
+                    Console.Write("Ange ett värde: ");
                     values[i] = Convert.ToInt32(Console.ReadLine());
                 }
                 catch (FormatException)
@@ -61,5 +67,71 @@ namespace Inlämningsuppgift2
                 }
             }
         }
+
+        //class get_input
+        //{
+
+        //    public static string Get_operator(string text)
+        //    {
+        //        int amount_operators = 0;
+        //        string allowed_operators = "+-*/", svar="hej";
+
+        //        string[] operators = new string[amount_operators];
+        //        int[] values = new int[amount_operators + 1];
+
+        //        for (int i = 0; i < amount_operators; i++)
+        //        {
+        //            do
+        //            {
+        //                try
+        //                {
+        //                    Console.Write("Ange operatör nr " + (i + 1) + ": ");
+        //                    operators[i] = Console.ReadLine();
+        //                    svar = operators[i];
+
+        //                }
+        //                catch (Exception)
+        //                {
+        //                    Console.Write("Du har angivet ett felaktivt värde. Vänligen skriv in en operatör: ");
+        //                    operators[i] = Console.ReadLine();
+        //                    svar = operators[i];
+
+        //                }
+        //            } while (operators[i] != allowed_operators && operators[i].Length != 1);
+        //        }
+        //        return svar;
+        //    }
+
+        //    public static int Get_value(int nummer)
+        //    {
+        //        int amount_operators = 0, svar = 0;
+
+        //        string[] operators = new string[amount_operators];
+        //        int[] values = new int[amount_operators + 1];
+
+        //        for (int i = 0; i < amount_operators + 1; i++)
+        //        {
+        //            try
+        //            {
+        //                Console.Write("Ange ett värde");
+        //                values[i] = Convert.ToInt32(Console.ReadLine());
+        //                svar = values[i];
+        //            }
+        //            catch (FormatException)
+        //            {
+        //                Console.WriteLine("Du har anget ett felaktivt värde, vänligen försök igen:");
+        //                values[i] = Convert.ToInt32(Console.ReadLine());
+        //                svar = values[i];
+        //            }
+        //        }
+        //        return svar;
+
+        //    }
+        //}
+
+        //class calculate
+        //{
+
+        //}
     }
 }
