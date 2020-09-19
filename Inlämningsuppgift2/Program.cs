ee78b96729aafc589eae6ba0c12968fd5bcba214
00joaand <00jooand@gafe.molndal.se>
2020-09-19 15:44:52 +0200

    flytade ut klasserna Get_input och Calculate utanför maklassen samt tog bort onödig indexering. Detta blir också första safety saven ifall jag skulle ändra på något bortom räddning

--- Inlämningsuppgift2/Program.cs
+++ Inlämningsuppgift2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.SymbolStore;
 using System.Dynamic;
@@ -11,9 +12,9 @@ namespace Inlämningsuppgift2
     {
         static void Main(string[] args)
         {
-            int amount_operators = 0, count=1, count2 = 1;
+            int amount_operators, count=1, count2 = 1;
 
-            amount_operators = get_input.operator_quantity();
+            amount_operators = Get_input.Operator_quantity();
 
             string[] operators = new string[amount_operators];
             int[] values = new int[amount_operators + 1];
@@ -21,83 +22,122 @@ namespace Inlämningsuppgift2
             for (int i = 0; i < amount_operators; i++)
             {
                 Console.Write("Ange operatör nr " + count + ": ");
-                operators[i] = get_input.Get_operator();
+                operators[i] = Get_input.Get_operator();
                 Console.WriteLine(operators[i]);
                 count++;
             }
             for (int i = 0; i < amount_operators + 1; i++)
             {
                 Console.Write("ange värde " + count2 + ": ");
-                values[i] = get_input.Get_value();
+                values[i] = Get_input.Get_value();
                 Console.WriteLine(values[i]);
                 count2++;
-            }
+            }                       
         }
-        class get_input
+    }
+    class Get_input
+    {
+        public static int Operator_quantity()
         {
-            public static int operator_quantity()
-            {
-                int svar = 0;
-                bool a = true;
+            int svar = 0;
+            bool a;
 
-                do
-                {
-                    try
-                    {
-                        Console.Write("Ange hur många operatörer du vill ha: ");
-                        svar = Convert.ToInt32(Console.ReadLine());
-                        a = false;
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Du har anget ett felaktivt värde, vänligen försök igen");
-                        a = true;
-                    }
-                } while (a != false);
-                return svar;
-            }
-            public static string Get_operator()
+            do
             {
-                string allowed_operators = "+-*/", svar = "hej";
-                bool a = true;
-
-                do
+                try
                 {
-                    svar = Console.ReadLine();
+                    Console.Write("Ange hur många operatörer du vill ha: ");
+                    svar = Convert.ToInt32(Console.ReadLine());
                     a = false;
-
-                    if (!allowed_operators.Contains(svar) || svar.Length != 1)
-                    {
-                        Console.Write("Du har angivit ett felaktivt värde, vänligen försök igen: ");
-                        a = true;
-                    }
-                } while (a != false);                
-                return svar;
-            }
-            public static int Get_value()
-            {
-                int svar = 0;
-                bool a = true;
-
-                do
+                }
+                catch (FormatException)
                 {
-                    try
-                    {
-                        svar = Convert.ToInt32(Console.ReadLine());
-                        a = false;
-                    }
-                    catch (FormatException)
-                    {
-                        Console.Write("du har anget ett felaktivt värde, vänligen försök igen:");
-                        a = true;
-                    }
-                } while (a != false);
-                return svar;
+                    Console.WriteLine("Du har anget ett felaktivt värde, vänligen försök igen");
+                    a = true;
+                }
+            } while (a != false);
+            return svar;
+        }
+        public static string Get_operator()
+        {
+            string allowed_operators = "+-*/", svar;
+            bool a;
+
+            do
+            {
+                svar = Console.ReadLine();
+                a = false;
+
+                if (!allowed_operators.Contains(svar) || svar.Length != 1)
+                {
+                    Console.Write("Du har angivit ett felaktivt värde, vänligen försök igen: ");
+                    a = true;
+                }
+            } while (a != false);                
+            return svar;
+        }
+        public static int Get_value()
+        {
+            int svar = 0;
+            bool a;
+
+            do
+            {
+                try
+                {
+                    svar = Convert.ToInt32(Console.ReadLine());
+                    a = false;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("du har anget ett felaktivt värde, vänligen försök igen:");
+                    a = true;
+                }
+            } while (a != false);
+            return svar;
+        }
+    }
+    class Calculate
+    {
+        public static int get_order(string svar)
+        {
+            switch (svar)
+            {
+                case "x":
+                    return 1;
+                case "/":
+                    return 2;
+                case "+":
+                    return 3;
+                case "-":
+                    return 4;
+                default:
+                    return 0;
             }
         }
-        class calculate
-        {
 
-        }
+        //public static double calc(int order)
+        //{
+        //    if (order == 1)
+        //    {
+
+        //    }
+        //    else if (order == 2)
+        //    {
+
+        //    }
+        //    else if (order == 3)
+        //    {
+
+        //    }
+        //    else if (order == 4)
+        //    {
+
+        //    }
+        //    else
+        //    {
+
+        //    }
+        //}
     }
 }
